068f8f376ebd79fb0952a9f3e1642f1beac02917
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-06-15 16:51:42 +0300

    Добавьте файлы проекта.

---
+++ CS8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CS8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message;
+            int numberRepetitions;
+            Console.Write("Введите сообщение: ");
+            message = Console.ReadLine();
+            Console.Write("Введите количество повторов сообщения: ");
+            numberRepetitions = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < numberRepetitions; i++)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
